623d86da8cf019d7bf761ca382d57bc19cacaee8
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 19:36:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  new_array = []
 array.each do |names|
-  badge_maker(names)
+  new_array.push(badge_maker(names))
 end
+return new_array
 end
