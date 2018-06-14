92adca04659bc2f09f6857a90e7a1de4bf7a583f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 19:54:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(array)
   new_array = []
 array.each_with_index do |names,index|
-  new_array.push("Hello #{names}, you'll be assigned room #{index + 1}!")
+  new_array.push("Hello #{names}! You'll be assigned to room #{index + 1}!")
 end
 return new_array
 end
